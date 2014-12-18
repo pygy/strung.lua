90b58228337cd52be3048238ad3bdf88e38385de
Pierre-Yves Gerardy <py@py.gy>
2014-12-18 15:42:05 +0100

    add some gsub tests (failing ATM)

--- tests/gsub.lua
+++ tests/gsub.lua
@@ -5,6 +5,8 @@ local try, gmtry, iter, allchars, dumpacc = c.try, c.gmtry, c.iter, c.allchars, 
 
 iter(10)
 
+-- basic tests
+
 try("gsub", "_d_d_", "d", "+")
 try("gsub", "_da_da_", "(d)a", "+")
 
@@ -14,5 +16,11 @@ try("gsub", "_d_d_", "d", {d = "9"})
 
 try("gsub", "_d_d_", "d", function()end)
 try("gsub", "_do_d_", "(d)(.)", function(a,b) return b,a end)
+
+-- the patterns used by f1() in pm.lua
+
+try("gsub", '(..*) %1', "%%([0-9])", function (s) return "%" .. (s+1) end)
+try("gsub", '(..*) %1', "^(^?)", "%1()", 1)
+try("gsub", '(..*) %1', "($?)$", "()%1", 1)
 
 if not bench then print"ok" end
